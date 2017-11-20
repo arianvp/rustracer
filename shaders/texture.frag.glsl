fede87daee97107df7cb0ee383e8fdef34152cb4
Arian van Putten <aeroboy94@gmail.com>
2017-11-20 19:15:01 +0100

    Get mandelbrot to work

--- shaders/texture.frag.glsl
+++ shaders/texture.frag.glsl
@@ -11,6 +11,30 @@ layout (location = 0) out vec4 f_color;
 
 void main() 
 {
-  f_color = vec4(1.0,0.0,0.0, 1.0);
+ // f_color = vec4(position.x,position.y,0.0, 1.0);
  // outFragColor = texture(samplerColor, inUV);
+
+
+
+
+
+
+    vec2 c = (position - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+
+    vec2 z = vec2(0.0, 0.0);
+    float i;
+    for (i = 0.0; i < 1.0; i += 0.005) {
+        z = vec2(
+            z.x * z.x - z.y * z.y + c.x,
+            z.y * z.x + z.x * z.y + c.y
+        );
+
+        if (length(z) > 4.0) {
+            break;
+        }
+    }
+
+    vec4 to_write = vec4(vec3(i), 1.0);
+    f_color = to_write;
+
 }
